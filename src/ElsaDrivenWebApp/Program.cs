c9930060c56a277709f3acf1d1072d012c7691fe
Frans van Ek <FransvanEk@Innax.nl>
2022-09-14 16:53:00 +0200

    almost fully implemented

--- src/ElsaDrivenWebApp/Program.cs
+++ src/ElsaDrivenWebApp/Program.cs
@@ -1,4 +1,9 @@
 using ElsaDrivenWebApp.Services;
+using DynamicBlazorForm.Core;
+using DynamicBlazorForm.Themes.HTML;
+using Newtonsoft.Json;
+using DynamicBlazorForm.Core.Layout;
+using DynamicBlazorForm.Core.Layout.FluentApi;
 
 var builder = WebApplication.CreateBuilder(args);
 var baseAddress = builder.Configuration["UsertaskService:BaseAddress"];
@@ -8,6 +13,10 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped(sp => new UsertaskService(new HttpClient { BaseAddress = new Uri(baseAddress) }));
 builder.Services.AddScoped(sp => new ProcessService(new HttpClient { BaseAddress = new Uri(baseAddress) }));
 
+//dynamic form
+builder.Services.AddScoped(sp =>
+    new DynamicElementsRepository()
+        .GetHTMLDefaultSettings());
 
 var app = builder.Build();
 
---
+++ src/Workflow.Samples/Sample3.cs
@@ -0,0 +1,39 @@
+﻿using Elsa.Activities.Console;
+using Elsa.Activities.Primitives;
+using Elsa.Activities.Signaling;
+using Elsa.Builders;
+using UserTask.AddOns;
+
+namespace Workflow.Samples
+{
+    internal class Sample3 : IWorkflow
+    {
+        public void Build(IWorkflowBuilder builder)
+        {
+            builder
+                .StartWith<SignalReceived>(a =>
+                {
+                    a.Set(x => x.Signal, "sample3");
+                    a.Set(x => x.Id, "SignalReceived3");
+
+                })
+                 .Then<SetVariable>(a =>
+                    {
+                        a.Set(x => x.VariableName, "Name");
+                        a.Set(x => x.Value, context =>  context.GetInput<Sample2Settings>());
+                    })
+                .SetName(context => ((Sample2Settings)context.GetVariable("Name")).Name)
+                .WriteLine($"Starting workflow")
+                .Then<UserTaskSignal>(a =>
+                {
+                    a.Set(x => x.Signal, "usertasksample3");
+                    a.Set(x => x.TaskName, "Demo Sample3");
+                    a.Set(x => x.TaskTitle, "Dynamic forms");
+                    a.Set(x => x.UIDefinition, "{\"groups\":[{\"name\":\"demo\",\"layoutHint\":\"\",\"subGroups\":[{\"layoutHint\":\"\",\"index\":1,\"items\":[{\"index\":1,\"span\":4,\"path\":\"$.demo1.test3\",\"typeName\":\"NumberInput\",\"layoutHint\":\"\",\"text\":\"leeftijd\",\"groups\":[],\"customData\":{}},{\"index\":2,\"span\":8,\"path\":\"$.demo1.test2\",\"typeName\":\"TextInput\",\"layoutHint\":\"\",\"text\":\"leeftijd in text\",\"groups\":[],\"customData\":{}}]},{\"layoutHint\":\"\",\"index\":2,\"items\":[{\"index\":1,\"span\":4,\"path\":\"$.demo1.test1\",\"typeName\":\"DateInput\",\"layoutHint\":\"\",\"text\":\"datum\",\"groups\":[],\"customData\":{}},{\"index\":2,\"span\":8,\"path\":\"$.demo1.test2\",\"typeName\":\"TextInput\",\"layoutHint\":\"\",\"text\":\"leeftijd in text\",\"groups\":[],\"customData\":{}}]}]}],\"title\":\"leeftijds vragen\"}");
+                    a.Set(x => x.TaskName, "The task will suspend the execution until the button is pressed");
+                }
+                )
+                .WriteLine($"Workflow is done");
+        }
+    }
+}
