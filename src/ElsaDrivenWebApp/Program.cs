e618510092998f04283eaa8af201aebeccdd54bf
Frans van Ek <FransvanEk@Innax.nl>
2022-10-10 12:51:16 +0200

    added styles and classes, refactored the statup and program files

--- src/ElsaDrivenWebApp/Program.cs
+++ src/ElsaDrivenWebApp/Program.cs
@@ -1,44 +1,15 @@
 using ElsaDrivenWebApp.Services;
 using DynamicBlazorForm.Core;
 using DynamicBlazorForm.Themes.HTML;
-using Newtonsoft.Json;
-using DynamicBlazorForm.Core.Layout;
-using DynamicBlazorForm.Core.Layout.FluentApi;
+using ElsaDrivenWebApp;
 
-var builder = WebApplication.CreateBuilder(args);
-var baseAddress = builder.Configuration["UsertaskService:BaseAddress"];
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddScoped(sp => new UsertaskService(new HttpClient { BaseAddress = new Uri(baseAddress) }));
-builder.Services.AddScoped(sp => new ProcessService(new HttpClient { BaseAddress = new Uri(baseAddress) }));
+var builder = Host.CreateDefaultBuilder(args) 
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+                webBuilder.UseSetting(WebHostDefaults.DetailedErrorsKey, "true");
+            });
 
-//dynamic form
-builder.Services.AddScoped(sp =>
-    new DynamicElementsRepository()
-        .GetHTMLDefaultSettings()
-        .Add("TextInput", typeof(TextInput))
-        .Add("NumberInput", typeof(NumberInput))
-        .Add("BoolInput", typeof(BoolInput))
-        .Add("DateInput", typeof(DateInput)));
+builder.Build().Run();
 
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
---
+++ src/ElsaDrivenWebApp/Startup.cs
@@ -0,0 +1,99 @@
+﻿using DynamicBlazorForm.Core;
+using DynamicBlazorForm.Themes.HTML;
+using ElsaDrivenWebApp.Services;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace ElsaDrivenWebApp
+{
+    public partial class Startup
+    {
+
+        private IConfiguration _configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        partial void OnConfigureServices(IServiceCollection services);
+        partial void OnConfiguringServices(IServiceCollection services);
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            OnConfiguringServices(services);
+
+            var baseAddress = _configuration["UsertaskService:BaseAddress"];
+            
+            // Add services to the container.
+            services.AddServerSideBlazor();
+            services.AddScoped(sp => new UsertaskService(new HttpClient { BaseAddress = new Uri(baseAddress) }));
+            services.AddScoped(sp => new ProcessService(new HttpClient { BaseAddress = new Uri(baseAddress) }));
+
+            //dynamic form
+            services.AddScoped(sp =>
+                new DynamicElementsRepository()
+                    .GetHTMLDefaultSettings()
+                    .Add("TextInput", typeof(TextInput))
+                    .Add("NumberInput", typeof(NumberInput))
+                    .Add("BoolInput", typeof(BoolInput))
+                    .Add("DateInput", typeof(DateInput)));
+
+
+            services.AddHttpContextAccessor();
+            services.AddScoped<HttpClient>(serviceProvider =>
+            {
+
+                var uriHelper = serviceProvider.GetRequiredService<NavigationManager>();
+
+                return new HttpClient
+                {
+                    BaseAddress = new Uri(uriHelper.BaseUri)
+                };
+            });
+
+            services.AddHttpClient();
+            services.AddRazorPages();
+
+            services.AddScoped<DialogService>();
+            services.AddScoped<NotificationService>();
+            services.AddScoped<TooltipService>();
+            services.AddScoped<ContextMenuService>();
+
+            OnConfigureServices(services);
+        }
+
+        partial void OnConfigure(IApplicationBuilder app, IWebHostEnvironment env);
+        partial void OnConfiguring(IApplicationBuilder app, IWebHostEnvironment env);
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            OnConfiguring(app, env);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.Use((ctx, next) =>
+                {
+                    return next();
+                });
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+
+            OnConfigure(app, env);
+        }
+
+    }
+}
